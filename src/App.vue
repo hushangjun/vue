22eaf44a5caeba8b2afbea720df0d17359ddbfff
GrimJ <2920722386@qq.com>
2017-07-20 16:18:41 +0800

    '初始化项目结构'

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+       {{test}}
+    </div>
+</template>
+<style scopted>
+  
+</style>
+<script> 
+    export default{
+        data:function(){
+            return{
+                test:200
+            }
+        }
+    }
+</script>
